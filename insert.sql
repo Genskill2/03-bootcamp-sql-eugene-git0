447cd4e12def3073021c106dfd19e320fba24003
eugene-git0 <84885609+eugene-git0@users.noreply.github.com>
2021-06-19 13:59:25 -0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,11 @@
+--publisher data
+insert into publisher (name, country) values ('PHI', 'India');
+insert into publisher (name, country) values ('Harper', 'USA');
+insert into publisher (name, country) values ('GCP', 'USA');
+insert into publisher (name, country) values ('Avery', 'USA');
+insert into publisher (name, country) values ('Del Rey', 'UK');
+insert into publisher (name, country) values ('Vintage', 'UK');
+
 --book data
 insert into books (title, publisher) values ('The C Programming Language', 1);
 insert into books (title, publisher) values ('The Go Programming Language', 1);
@@ -7,13 +15,7 @@ insert into books (title, publisher) values ('Deep Work', 3);
 insert into books (title, publisher) values ('Atomic Habits', 4);
 insert into books (title, publisher) values ('The City and The City', 5);
 insert into books (title, publisher) values ('The Great War for Civilisation', 6);
---publisher data
-insert into publisher (name, country) values ('PHI', 'India');
-insert into publisher (name, country) values ('Harper', 'USA');
-insert into publisher (name, country) values ('GCP', 'USA');
-insert into publisher (name, country) values ('Avery', 'USA');
-insert into publisher (name, country) values ('Del Rey', 'UK');
-insert into publisher (name, country) values ('Vintage', 'UK');
+
 --subject data
 insert into subjects (name) values ('C');
 insert into subjects (name) values ('UNIX');
@@ -24,29 +26,39 @@ insert into subjects (name) values ('Productivity');
 insert into subjects (name) values ('Politics');
 insert into subjects (name) values ('Psychology');
 insert into subjects (name) values ('History');
+
 --books subjects data
+
 -- The C programming language
+
 insert into books_subjects (book, subject) values (1, 1);
 insert into books_subjects (book, subject) values (1, 2);
 insert into books_subjects (book, subject) values (1, 3);
 --Go programming Language
+
 insert into books_subjects (book, subject) values (2, 4);
 insert into books_subjects (book, subject) values (2, 3);
 --The UNIX Programming Environment
+
 insert into books_subjects (book, subject) values (3, 2);
 insert into books_subjects (book, subject) values (3, 3);
 --Cryptonomicon
+
 insert into books_subjects (book, subject) values (4, 3);
 insert into books_subjects (book, subject) values (4, 5);
 --Deep Work
+
 insert into books_subjects (book, subject) values (5, 3);
 insert into books_subjects (book, subject) values (5, 6);
 --Atomic Habits
+
 insert into books_subjects (book, subject) values (6, 6);
 insert into books_subjects (book, subject) values (6, 8);
 --The City and The City
+
 insert into books_subjects (book, subject) values (7, 5);
 insert into books_subjects (book, subject) values (7, 7);
 --The Great War for Civilisation
+
 insert into books_subjects (book, subject) values (8, 7);
 insert into books_subjects (book, subject) values (8, 9);
